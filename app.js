f6296689a463c4f12a6e05c6933065e67e18aaf0
lltemplar <liaglin@163.com>
2017-07-09 11:24:57 +0800

    'test'

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var schedule = require('node-schedule');
 var exec = require('child_process').exec;
+var cmd = require('./script.js');
 
 
 /*  *  *  *  *  *  *
@@ -16,8 +17,10 @@ var exec = require('child_process').exec;
 function scheduleCronstyle() {
     schedule.scheduleJob('*/5 * * * * *', function () {
         console.log('scheduleCronstyle:' + new Date());
-        exec('echo hello world', function (error, stdout, stderr) {
+        console.log(cmd);
+        exec(cmd, function (error, stdout, stderr) {
             console.dir(stdout);
+            console.error(stderr);
         });
     });
 }
--- script.js
+++ script.js
@@ -1,4 +1,13 @@
- var script = [
-     "git pull",
-     "git "
-     ].join(';');
+var script = [
+    "git pull origin master",
+    "git add .",
+    "git commit -m 'commit code in " + getDate()+"'",
+    "git push"
+].join('&');
+
+function getDate() {
+    var d = new Date();
+    return d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate() + ' ' + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+}
+
+module.exports = script;    
