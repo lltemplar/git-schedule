d893a70e4ad85498e4c38076373bc850958fd667
lltemplar <liaglin@163.com>
2017-07-09 23:23:45 +0800

    commit code in 2017-7-9 23:23:40

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var cmd = require('./script.js');
 */
 
 function scheduleCronstyle() {
-    schedule.scheduleJob('0 0 * * * *', function () {
+    schedule.scheduleJob('*/5 * * * * *', function () {
         console.log('scheduleCronstyle:' + new Date());
         delete require.cache['./script.js'];
         
